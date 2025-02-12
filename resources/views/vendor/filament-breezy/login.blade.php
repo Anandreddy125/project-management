e631be1d779c5a0e79b430141b515fd8be7309ec
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2025-02-12 16:34:31 +0530

    Update login.blade.php

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <h2 class="font-bold tracking-tight text-center text-2xl">
-        Login page updated testing last version
+        Login page updated private dcoker image pull and deploying k3s
     </h2>
 
     @if(session()->has('oidc_error'))
