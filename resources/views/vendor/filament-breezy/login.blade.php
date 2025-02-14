e1f73f401d9e995997179fea77c9655175f5cfb4
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2025-02-14 15:11:56 +0530

    Update login.blade.php

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <h2 class="font-bold tracking-tight text-center text-2xl">
-        Login page for sanand
+        Login page for anrs
     </h2>
 
     @if(session()->has('oidc_error'))
