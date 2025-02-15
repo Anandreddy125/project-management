794ab342c86eabac8a793b41b1d6c12a073c0542
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2025-02-15 14:29:42 +0530

    Update login.blade.php

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <h2 class="font-bold tracking-tight text-center text-2xl">
-        Login page aaaaaa
+        Login page 345678
     </h2>
 
     @if(session()->has('oidc_error'))
