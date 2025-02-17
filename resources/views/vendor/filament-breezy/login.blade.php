e895be8ca54aa7e58d3cff92a59e59aec4dfdbd0
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2025-02-17 17:06:24 +0530

    Update login.blade.php

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <h2 class="font-bold tracking-tight text-center text-2xl">
-        Login sample-page git commit testing
+        Login sample-page 
     </h2>
 
     @if(session()->has('oidc_error'))
