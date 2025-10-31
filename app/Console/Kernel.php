57d840e487b43b4afa851885bcf3a27f8e2f4156
ANAND <ANAND@PROPHZ.com>
2025-10-31 16:50:39 +0530

    docker image

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel merge
+class Kernel extends ConsoleKernel final test for docker build image
 {
     /**
      * Define the application's command schedule. 
