e0f1e27de7b6872d86252cf1566f4abdf87e8ddf
ANAND <ANAND@PROPHZ.com>
2025-10-31 15:37:42 +0530

    final

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel final one morest
+class Kernel extends ConsoleKernel finale
 {
     /**
      * Define the application's command schedule. 
