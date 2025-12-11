f60882200f8a1342fadd4bc0cd83ad27f0c48209
ANAND <ANAND@PROPHZ.com>
2025-12-11 14:30:08 +0530

    jdgcvn b

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,4 +32,4 @@ class Kernel extends ConsoleKernel final test for docker build image
         require base_path('routes/console.php');
     }
 }
-//AWS costs are based on a pay-as-you-go utility model for individual services, driven primarily by compute, storage, and outbound data transfer. The key tools for managing and reporting these costs are AWS Cost Explorer and the detailed AWS Cost and Usage Reports (CUR). kjchkzsdjfcbsjfhvbdjxf.jhrgdcsuzdjchghwmesgcuesjgfvesjdrgnvcesujyfgcbseouryfdgbp;QIKQUWSJqoik2euj  kwjeaydgkwuefgeksurtchgeksuyrshmgdkweuzsjdgchkusdrzjfhg vekuszrhdfg bwuaekjfdcgblwiakmjsahzblI   MJ
+//AWS costs are based on a pay-as-you-go utility model for individual services, nbcv nxcb xnbcv jznxn bc ,kjNX,KJCV J  HJ MQBDACdriven primarily by compute, storage, and outbound data transfer. The key tools for managing and reporting these costs are AWS Cost Explorer and the detailed AWS Cost and Usage Reports (CUR). kjchkzsdjfcbsjfhvbdjxf.jhrgdcsuzdjchghwmesgcuesjgfvesjdrgnvcesujyfgcbseouryfdgbp;QIKQUWSJqoik2euj  kwjeaydgkwuefgeksurtchgeksuyrshmgdkweuzsjdgchkusdrzjfhg vekuszrhdfg bwuaekjfdcgblwiakmjsahzblI   MJ
