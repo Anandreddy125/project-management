b516e3f51114a88df77cda1d99f4715c56c703de
AnandR1225 <anand.reddy@prophaze.com>
2025-12-16 11:23:00 +0530

    made some change on the jenkinsfile

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,3 +38,4 @@ class Kernel extends ConsoleKernel final test for docker build image
 //testing error on jenkinsq
 //tagging not working and webhook also
 // the jenkinsfile change 
+// made some change on the jenkinsfile
